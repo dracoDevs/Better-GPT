707ec013345edcd7e2949233bda1b0e46d89eefc
dracoDevs <67403295+dracoDevs@users.noreply.github.com>
2024-08-05 14:07:48 -0400

    Create BetterGPT.plugin.js

---
+++ BetterGPT.plugin.js
@@ -0,0 +1,14 @@
+/**
+ * @name Better GPT
+ * @author drcoo
+ * @description A streamlined wrapper of the OpenAI GPT-4 API for Better Discord message assistance.
+ * @version 0.0.1
+ */
+
+module.exports = class MyPlugin {
+    constructor(meta) {}
+
+    start() {}
+
+    stop() {}
+};
